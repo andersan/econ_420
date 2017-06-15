5d9918db44fe46776f4b5cfd20885cf27064068c
cree <andersan@umich.edu>
2017-06-14 23:27:17 -0400

    change variable names to be intuitive. change name of file to be able to import it.

---
+++ data.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+from os import chdir
+import glob
+import test
+
+#improvements: change variable names to be more intuitive
+
+chdir("/home/cree/Downloads/br_econ/")
+count = 0
+mun, state = {}, {}
+
+mun = import_data("mun")
+state = import_data("state")
+
+# municipal dataframes
+municipio_renda = mun.get('mun/Renda_municipios - Renda familiar - per capita - media.csv')
+municipio_ensino = mun.get('mun/mun_media_anos_de_estudos_25_anos_+_todos.csv')
+municipio_saude = mun.get('mun/mun_Mortalidade infantil (por mil nascidos vivos)_1970-2000.csv') 
+municipio_populacao_censo	= mun.get('mun/populacao_municipal.csv')
+
+#state dataframes
+state_renda = state.get('state/estado_Renda domiciliar per capita - media_1976 - 2014.csv')
+state_ensino = state.get('state/Anos de estudo - media - pessoas 25 anos e mais - 1981 - 2014.csv')
+state_ensino_mulheres = state.get('state/Anos de estudo - media - pessoas 25 anos e mais - mulheres 1981 -2014.csv')
+state_populacao_anual = state.get('state/populcao_estado_1980-2014.csv')
+state_populacao_censo = state.get('state/populacao_residente_estado_1970-2000.csv')
+
+#calcular mortalidade infantil por estado
+df9 = df8[['Sigla', 'Código', 'Estado']].copy()
+df10 = df2.drop('Código', 1).groupby('Sigla').mean()
+df10.reset_index(level='Sigla', inplace=True)
+
+state_saude = pd.merge(df9,df10, on='Sigla')
--- econ.py
+++ econ.py
@@ -2,24 +2,24 @@ import pandas as pd
 import numpy as np
 from os import chdir
 import statsmodels.api as sm
-import test
+import data
 
 chdir("/home/cree/Downloads/br_econ/")
 
 # municipal: loop through income and years of study
-test.OLS(test.df,test.df1)
+test.OLS(test.municipio_renda,test.municipio_ensino)
 
 # municipal: loop through infant mortality and years of study
-test.OLS(test.df1,test.df2)
+test.OLS(test.municipio_ensino,test.municipio_saude)
 
 # municipal: loop through income and infant mortality
-test.OLS(test.df,test.df2)
+test.OLS(test.municipio_saude,test.municipio_renda)
 
 # state: loop through income and years of study
-test.OLS(test.df4,test.df5)
+test.OLS(test.state_renda,test.state_ensino)
 
 # state: loop through infant mortality and years of study
-test.OLS(test.df5,test.df9)
+test.OLS(test.state_saude,test.state_ensino)
 
 # state: loop through income and infant mortality
-test.OLS(test.df4,test.df9)
+test.OLS(test.state_renda,test.state_saude)
